62e725d336dfe8835c36c43620d292e3e06416ac
options3224@gmail.com <options3224@gmail.com>
2023-02-21 00:10:09 +0900

    컨트롤러 실습 완료

---
+++ WebApplication1/WebApplication1/Controllers/BoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers {
+    public class BoardController : Controller {     // 컨트롤러
+        public string List(int? id) {                // 액션
+            if (id == null)
+                return "Error!!!";
+
+            return $"Board id : {id.Value}";
+        }
+    }
+}
