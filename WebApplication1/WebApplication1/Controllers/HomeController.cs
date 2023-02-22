4095546495dca235f211260274a404b669a322b6
options3224@gmail.com <options3224@gmail.com>
2023-02-22 16:06:00 +0900

    Razor 실습 완료

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers {
     public class HomeController : Controller {
-        /*
         public IActionResult Index() {
+            ViewBag.Arr = new string[] { "alpha", "beta", "gamma" };
+            ViewBag.Book = new Book {
+                Title = "칼의 노래",
+                Writer = "김훈"
+            };
             return View();
         }
-        */
 
+        /*
         public string Index() {
             return "Hello World!";
         }
+        */
     }
 }
---
+++ WebApplication1/WebApplication1/Models/Book.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Models {
+    public class Book {
+        public string Title { get; set; }
+        public string Writer { get; set; }
+    }
+}
