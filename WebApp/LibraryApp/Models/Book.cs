7601b1cc934e50b4158ee91cd129da0cbbfefb61
options3224@gmail.com <options3224@gmail.com>
2023-02-22 16:49:13 +0900

    Book 모델 정의(Key 포함)

---
+++ WebApp/LibraryApp/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.Models {
+    // Book 모델
+    public class Book {
+        // MS-SQL과의 연동을 위한 Key 명시
+        [Key] public int Book_U { get; set; }
+        public string Title { get; set; }
+        public string Writer { get; set; }
+        public string Summary { get; set; }
+        public string Publisher { get; set; }
+        public int Published_date { get; set; }
+
+    }
+}
