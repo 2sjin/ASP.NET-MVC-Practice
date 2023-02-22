812559afdbfb4c8a25314bd590f293d5cfe98ce2
options3224@gmail.com <options3224@gmail.com>
2023-02-22 16:36:19 +0900

    솔루션 이름 변경(-> WebApp), 레이아웃 실습 완료

---
+++ WebApp/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Alpha() {
+            return View();
+        }
+
+        public IActionResult Beta() {
+            return View();
+        }
+    }
+}
