bd596fb95f2c95b5ac0db8aca231e7605f98ed76
options3224@gmail.com <options3224@gmail.com>
2023-02-23 12:56:19 +0900

    주석 일부 수정, Book 모델에 DisplayName 추가, Index 뷰의 items 출력 부분 소스코드 간소화

--- WebApp/LibraryApp/Models/Book.cs
+++ WebApp/LibraryApp/Models/Book.cs
@@ -1,14 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibraryApp.Models {
+    // MS-SQL의 테이블에 해당하는 클래스
     public class Book {
-        // MS-SQL과의 연동을 위한 Key 명시
-        [Key]
+        // Book 테이블의 열(속성)에 해당하는 프로퍼티
+        [Key]   // 기본키
         public int Book_U { get; set; }
+
+        [DisplayName("제목")]
         public string Title { get; set; }
+
+        [DisplayName("저자")]
         public string Writer { get; set; }
+
+        [DisplayName("요약")]
         public string Summary { get; set; }
+
+        [DisplayName("출판사")]
         public string Publisher { get; set; }
+
+        [DisplayName("출판일")]
         public int Published_date { get; set; }
     }
 }
