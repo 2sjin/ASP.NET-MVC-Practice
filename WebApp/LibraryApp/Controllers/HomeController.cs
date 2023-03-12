6bb16f30d5d2db1b1af20cc1f7b050fd081708e9
options3224@gmail.com <options3224@gmail.com>
2023-03-12 23:05:40 +0900

    컨트롤러 내 쿼리스트링 저장 방식 수정, 검색창 키워드가 반영 안되는 오류 해결

--- WebApp/LibraryApp/Controllers/HomeController.cs
+++ WebApp/LibraryApp/Controllers/HomeController.cs
@@ -26,12 +26,14 @@ namespace LibraryApp.Controllers {
             IQueryable<Book> books;     // 출력할 도서 목록을 저장할 쿼리 인터페이스
 
             // 쿼리 스트링을 배열 형태로 저장
-            // 예) ["?page", 1]
-            string[] queryStr = Request.QueryString.ToString().Split("=");  
+            // 예) ["", "searchKind", "Title", "page", 1]
+            string[] queryStr = Request.QueryString.ToString().Split(new char[] { '?', '&', '=' });
+            foreach (string s in queryStr)
+                Console.WriteLine(">> " + s);
 
             // 쿼리스트링의 파라미터가 'page'인 경우, 쿼리스트링 값(현재 페이지)을 정수 형태로 저장
-            if (queryStr[0] == "?page" && queryStr[1] != null)
-                pageNum = Convert.ToInt32(queryStr[1]);
+            if (queryStr.Length >= 2 && queryStr[1] == "page" && queryStr[2] != null)
+                pageNum = Convert.ToInt32(queryStr[2]);
 
             // 현재 페이지의 첫 레코드의 기본키를 갱신함
             firstRecordNum = (pageNum - 1) * listCount;
@@ -40,9 +42,9 @@ namespace LibraryApp.Controllers {
             ViewBag.PageNum = pageNum;
 
             // 검색어 입력에 따른 필터링(책제목 기준)
-            if (queryStr[0] == "?keyword" && !string.IsNullOrWhiteSpace(queryStr[1])) {
+            if (queryStr.Length >= 4 && queryStr[3] == "keyword" && !string.IsNullOrWhiteSpace(queryStr[4])) {
                 // URL 인코딩(퍼센트 인코딩) 형태의 문자열을 (한글 깨짐 해결)
-                string queryStrDecode = HttpUtility.UrlDecode(queryStr[1]);
+                string queryStrDecode = HttpUtility.UrlDecode(queryStr[4]);
                 books = _context.Book.Where(x => x.Title.Contains(queryStrDecode));
                 ViewBag.PageNumMax = (int)((books.Count() - 1) / listCount) + 1;     // 최대 페이지 수를 뷰에 전달(ViewBag)
                 books = books.OrderBy(x => x.Book_U)
